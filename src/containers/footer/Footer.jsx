920da29e71bf9b6f6455d0e81ae7a8068605b96f
Charles <knappcharles@icloud.com>
2023-01-22 22:28:07 -0700

    Minor footer fixes

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => (
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Your super-powered personal assistant. OpenAI is creating artificial intelligence that helps humans complete tasks.</p>
         <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
@@ -40,7 +41,7 @@ const Footer = () => (
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2023 GPT-3. All rights reserved.</p>
     </div>
   </div>
 );
